d917a174e86eb0240742137a3e07babb248b0a6e
rjwright <rjwright@chromium.org>
2014-11-03 13:40:23 +1100

    CamelCase properties

--- src/position-handler.js
+++ src/position-handler.js
@@ -109,6 +109,6 @@
   }
 
   var mergePositionList = scope.mergeNestedRepeated.bind(null, mergeOffsetList, ', ');
-  scope.addPropertiesHandler(parsePositionList, mergePositionList, ['background-position', 'object-position']);
+  scope.addPropertiesHandler(parsePositionList, mergePositionList, ['backgroundPosition', 'objectPosition']);
 
 })(webAnimationsMinifill);
