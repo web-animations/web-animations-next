8b618c8eefe9a2e9fd8b3af443023ffc332850f6
Sam Thorogood <sam.thorogood@gmail.com>
2014-11-14 10:07:48 +1100

    added group iteration tests

---
+++ test/js/timing.js
@@ -0,0 +1,25 @@
+suite('timing-tests', function() {
+  setup(function() {
+    document.timeline._players = [];
+  });
+
+  test('animation iterations', function() {
+    var anim = new Animation(document.body, [], {
+      duration: 2000,
+      iterations: 2,
+      iterationStart: 0.5
+    });
+    assert.equal(anim.activeDuration, 4000);
+  });
+
+  test('group iterations', function() {
+    var anim = new Animation(document.body, [], {
+      duration: 2000
+    });
+    var group = new AnimationGroup([anim], {
+      iterations: 2,
+      iterationStart: 0.5
+    });
+    assert.equal(group.activeDuration, 4000);
+  });
+});
