8a9761e9e0de6c3469b2fad9f28471cb65457244
Sam Thorogood <sam.thorogood@gmail.com>
2014-11-14 10:09:59 +1100

    note about why it fails

--- test/js/timing.js
+++ test/js/timing.js
@@ -18,7 +18,9 @@ suite('timing-tests', function() {
     });
     var group = new AnimationGroup([anim], {
       iterations: 2,
-      iterationStart: 0.5
+      iterationStart: 0.5/*,
+      duration: 2000*/
+      // note: fails even with explicit duration here, as opposed to 'auto'
     });
     assert.equal(group.activeDuration, 4000);
   });
