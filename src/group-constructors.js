0c1b77579a95072cc60a884461e84828c397e114
shans <shane.stephens@gmail.com>
2015-01-22 16:27:13 +1100

    Revert "Don't tick a group if its player is in the pending state."

--- src/group-constructors.js
+++ src/group-constructors.js
@@ -55,8 +55,6 @@
     var underlyingPlayer;
     var ticker = function(tf) {
       var player = underlyingPlayer._wrapper;
-      if (player.playState == 'pending') return;
-
       if (!player.source)
         return;
       if (tf == null) {
